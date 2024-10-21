39f3b85733538751330f8c280936eec7abb700fa
Yash <niwaneyash@gmail.com>
2024-10-21 23:14:45 +0530

    price update

--- src/data/products.js
+++ src/data/products.js
@@ -2,56 +2,56 @@ export const products = [
   {
     name: 'मोतीचुर लाडू',
     description: 'गोड आणि मऊ मोतीचुर लाडू, परंपरागत पद्धतीने बनवलेले.',
-    price: '400/kg',
+    price: '399/kg',
     imageUrl: 'src/assets/Motichur.jpg',
     phoneNumber: '917038719588'
   },
   {
     name: 'शंकरपाळी गोड',
     description: 'कुरकुरीत आणि गोड शंकरपाळी, चहासोबत परफेक्ट.',
-    price: '400/kg',
+    price: '399/kg',
     imageUrl: 'src/assets/Shankharpadi.jpg',
     phoneNumber: '917038719588'
   },
   {
     name: 'चकली',
     description: 'तांदळाच्या पिठापासून बनवलेली कुरकुरीत चकली.',
-    price: '400/kg',
+    price: '399/kg',
     imageUrl: 'src/assets/chakli.webp',
     phoneNumber: '917038719588'
   },
   {
     name: 'शंकरपाळी खारी',
     description: 'खारट आणि कुरकुरीत शंकरपाळी, नाश्त्यासाठी उत्तम.',
-    price: '400/kg',
+    price: '399/kg',
     imageUrl: 'src/assets/khari.jpg',
     phoneNumber: '917038719588'
   },
   {
     name: 'करंजी',
     description: 'गोड भरलेली कुरकुरीत करंजी, दिवाळीची खास मिठाई.',
-    price: '400/kg',
+    price: '399/kg',
     imageUrl: 'src/assets/karanji.png',
     phoneNumber: '917038719588'
   },
   {
     name: 'बालुशाही',
     description: 'मऊ आणि गोड बालुशाही, चहासोबत आनंद घ्या.',
-    price: '400/kg',
+    price: '399/kg',
     imageUrl: 'src/assets/balushai.jpeg',
     phoneNumber: '917038719588'
   },
   {
     name: 'मिक्स चिवडा',
     description: 'विविध प्रकारचे पोहे, शेव, आणि मसाले यांचे टेस्टी मिश्रण.',
-    price: '400/kg',
+    price: '399/kg',
     imageUrl: 'src/assets/Mixchivda.jpg',
     phoneNumber: '917038719588'
   },
   {
     name: 'बेसन लाडू',
     description: 'शुद्ध तूप आणि बेसनापासून बनवलेले पारंपारिक लाडू.',
-    price: '400/kg',
+    price: '399/kg',
     imageUrl: 'src/assets/besanladdu.jpg',
     phoneNumber: '917038719588'
   }
